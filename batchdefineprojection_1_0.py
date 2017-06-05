00cb993b702619f81d16375a72621cff4232275a
bengappa <Ben.Gappa@gmail.com>
2017-06-05 13:28:46 -0500

    Initial Commit - Python File
    Adding the Python file to the initial commit.

---
+++ batchdefineprojection_1_0.py
@@ -0,0 +1,55 @@
+#
+#       A Simple batch Define Projection tool
+#       Made for the C-MAP Minneapolis QC Team
+#       
+#       Benjamin Gappa
+#       2/4/17
+#
+#       Adding optional parameter to add files
+#       to map document
+#       2/8/17
+#
+
+# Modules
+import arcpy
+from arcpy import env
+
+# Inputs
+in_folder = arcpy.GetParameterAsText(0)
+prj = arcpy.GetParameterAsText(1) # "4326" # Code for WGS 84
+add_layer = arcpy.GetParameterAsText(2) # Gets added to Arc Script tool as optional parameter, Boolean type
+
+# Workspace
+env.workspace = in_folder
+env.overwriteOutput = True
+# Map document
+mxd = arcpy.mapping.MapDocument("CURRENT")
+df = arcpy.mapping.ListDataFrames(mxd)[0]
+
+# Counter
+i = 0
+
+try:
+    # Loop
+    for fc in arcpy.ListFeatureClasses():
+
+        i += 1
+        # Define Projection
+        arcpy.DefineProjection_management(fc, prj)
+        arcpy.AddMessage("Projection Defined for " + fc + ".")
+
+        if str(add_layer) == "true":
+            # Add the file to map document
+            new_layer = arcpy.mapping.Layer(in_folder + "\\" + fc)
+            arcpy.mapping.AddLayer(df, new_layer)
+
+    arcpy.AddMessage("Projection Defined for " + str(i) + " shapefiles.")
+
+
+
+except:
+    # If tool does not execute, do these
+    arcpy.AddError("The Define Projection script did not execute properly.")
+    arcpy.AddMessage(arcpy.GetMessages())
+
+
